90c3957477b66bd63d590d58f5b3a4ff49113188
Adam Chalkley <atc0005@users.noreply.github.com>
2022-03-18 06:57:24 -0500

    WIP: Add 'implements assertion' for NullString type

--- adaptivecard/nullstring.go
+++ adaptivecard/nullstring.go
@@ -20,6 +20,18 @@ import (
 // https://stackoverflow.com/questions/31048557/assigning-null-to-json-fields-instead-of-empty-strings
 // https://stackoverflow.com/questions/25087960/json-unmarshal-time-that-isnt-in-rfc-3339-format/
 
+// Add an "implements assertion" to fail the build if the json.Unmarshaler
+// implementation isn't correct.
+//
+// This resolves the unparam linter error:
+// (*NullString).UnmarshalJSON - result 0 (error) is always nil (unparam)
+//
+// https://github.com/mvdan/unparam/issues/52
+var _ json.Unmarshaler = (*NullString)(nil)
+
+// Perform similar "implements assertion" for the json.Marshaler interface.
+var _ json.Marshaler = (*NullString)(nil)
+
 // NullString represents a string value used in component fields that may
 // potentially be null in the input JSON feed.
 type NullString string
@@ -28,14 +40,12 @@ type NullString string
 // custom Unmarshaler implementation to handle potentially null component
 // description field value.
 func (ns NullString) MarshalJSON() ([]byte, error) {
-
 	if len(string(ns)) == 0 {
 		return []byte("null"), nil
 	}
 
 	// NOTE: If we fail to convert the type, an infinite loop will occur.
 	return json.Marshal(string(ns))
-
 }
 
 // UnmarshalJSON implements the json.Unmarshaler interface to handle
